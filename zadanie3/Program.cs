fcf4ac8fece012d523bd346809beab8f12588e5d
Pavel Popkov <goil2011@yandex.ru>
2022-08-19 21:41:59 +0300

    задача3

---
+++ zadanie3/Program.cs
@@ -0,0 +1,20 @@
+﻿double[] array = new double [6];
+for (int i = 0; i < 6; i++)
+    array[i] = new Random().Next(1, 100);
+double max = array[0];
+double min = array[0];
+double raz = 0;
+Console.WriteLine("[" + string.Join(", ", array) + "]" ) ;
+for (int i = 0; i < 6; i++)
+{
+    if ( array[i] > max)
+        
+        max = array[i];
+
+    if (array[i] < min)
+        min = array[i];
+
+        raz = max - min;
+
+}   
+Console.WriteLine("Разница между максимальным и минимальным равна: " + raz);
