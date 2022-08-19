a8227e951f572e2ed1d99a5cbf4ede4c6ec611b2
Pavel Popkov <goil2011@yandex.ru>
2022-08-19 21:06:54 +0300

    задача2

---
+++ zadanie2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int [10];
+for (int i = 0; i < 10; i++)
+     array[i] = new Random().Next(1, 10);
+int NECH = 0;
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+for (int i = 0; i < 10; i++)
+{
+    if ( i % 2 != 0)
+
+        NECH = NECH + array[i];
+    
+            
+}
+Console.WriteLine("Сумма нечетных элементов массива равна: " + NECH);
