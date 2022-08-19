22e51af8f7f7b7797a0a95facbf00fb51db61737
Pavel Popkov <goil2011@yandex.ru>
2022-08-19 19:03:52 +0300

    задача 1

---
+++ zadanie1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int [6];
+for (int i = 0; i < 6; i++)
+     array[i] = new Random().Next(100, 1000);
+int sum_honest = 0;
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+for (int i = 0; i < 6; i++)
+{
+    if (array[i] % 2 == 0)
+        sum_honest = sum_honest+1;
+}
+Console.Write("Колличество четсных элементов массива равно: ");
+Console.Write(sum_honest);
